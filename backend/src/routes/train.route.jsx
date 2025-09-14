18cf02ffacdea7f91216dbf52f7b5c3a18a3dcce
Debraj Ghosh <rajdeb2522a@gmail.com>
2025-09-14 20:25:53 +0530

    change file name

--- backend/src/routes/train.route.jsx
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getTrains, createTrain } from "../controllers/trains.controller.jsx";
-
-const router = Router();
-
-router.get("/", getTrains);
-router.post("/", createTrain);
-
-export default router;
